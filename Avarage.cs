30f4c03ce608567e94f8c136bc3a6cdaaf8f52f9
Gulchin20 <116623821+Gulchin20@users.noreply.github.com>
2022-10-31 23:12:23 +0400

    flowchart commit1

---
+++ Avarage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hgydf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int i;
+            int sayac = -1;
+            double arrage;
+            for (i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum = sum + i;
+                    sayac++;
+
+                }
+
+            }
+            Console.WriteLine(arrage = (sum / sayac));
+
+        
+        }
+    }
+}
---
+++ Natural.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tekonluq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 122;
+            int rmain;
+            int sum=0;
+            while (num > 0)
+            {
+                rmain = num % 10;
+                sum=sum+rmain;
+                num = num / 10;
+                
+            }
+            
+
+            Console.WriteLine(sum);
+        }
+    }
+}
